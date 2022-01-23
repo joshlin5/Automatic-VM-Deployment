219d668da475a9ed1cd51f992e5036c5697905b4
Chris Parnin <chris.parnin@gmail.com>
2022-01-23 13:59:13 -0500

    wip: extract classes, move code.

--- commands/up.js
+++ commands/up.js
@@ -1,14 +1,7 @@
 const chalk = require('chalk');
-const fs    = require('fs');
-const os    = require('os');
-const path  = require('path');
-const waitssh = require('waitssh');
 
-
-const virtcrud = require('virtcrud');
-
-const VBoxManage = require('../lib/VBoxManage');
-const ssh = require('../lib/ssh');
+const VBoxProvider = require('../lib/provider/vbox');
+const VirtualizationFrameworkProvider = require('../lib/provider/vf');
 
 exports.command = 'up';
 exports.desc = 'Provision and configure a new development environment';
@@ -27,91 +20,19 @@ exports.builder = yargs => {
 exports.handler = async argv => {
     const { force, processor } = argv;
 
-    (async () => {
-    
+    let provider = new VBoxProvider()
 
+    if( processor === "Arm64" ) {
+        provider = new VirtualizationFrameworkProvider();
+    }
 
-        if( processor === "Arm64" ) {
-            
-        } else {
-            await up(force);
-        }
-
-    })();
-
-};
-
-async function up(force)
-{
-    // Use current working directory to derive name of virtual machine
-    let cwd = process.cwd().replace(/[/]/g,"-").replace(/\\/g,"-");
-    let name = `V`;    
+    let name = `V`;
     console.log(chalk.keyword('pink')(`Bringing up machine ${name}`));
 
-    // We will use the image we've pulled down with bakerx.
-    let image = path.join(os.homedir(), '.bakerx', '.persist', 'images', 'focal', 'box.ovf');
-    if( !fs.existsSync(image) )
-    {
-        console.log(chalk.red(`Could not find ${image}. Please download with 'bakerx pull ${image} cloud-images.ubuntu.com'.`))
-    }
-
-    // We check if we already started machine, or have a previous failed build.
-    let state = await VBoxManage.show(name);
-    console.log(`VM is currently: ${state}`);
-    if( state == 'poweroff' || state == 'aborted' || force) {
-        console.log(`Deleting powered off machine ${name}`);
-        // Unlock
-        await VBoxManage.execute("startvm", `${name} --type emergencystop`).catch(e => e);
-        await VBoxManage.execute("controlvm", `${name} --poweroff`).catch(e => e);
-        // We will delete powered off VMs, which are most likely incomplete builds.
-        await VBoxManage.execute("unregistervm", `${name} --delete`);
-    }
-    else if( state == 'running' )
-    {
-        console.log(`VM ${name} is running. Use 'V up --force' to build new machine.`);
-        return;
-    }
-
-    // Import the VM using the box.ovf file and register it under new name.
-    await VBoxManage.execute("import", `"${image}" --vsys 0 --vmname ${name}`);
-    // Set memory size in bytes and number of virtual CPUs.
-    await VBoxManage.execute("modifyvm", `"${name}" --memory 1024 --cpus 1`);
-    // Disconnect serial port
-    await VBoxManage.execute("modifyvm", `${name}  --uart1 0x3f8 4 --uartmode1 disconnected`);
-
-    // Run your specific customizations for the Virtual Machine.
-    await customize(name);
-
-    // Start the VM.
-    // Unlock any session.
-    await VBoxManage.execute("startvm", `${name} --type emergencystop`).catch(e => e);
-    // Real start.
-    await VBoxManage.execute("startvm", `${name} --type headless`);
-
-    // Explicit wait for boot
-    let sshInfo = {port: 2800, hostname: 'localhost'}
     try {
-        console.log(`Waiting for ssh to be ready on localhost:2800...`);        
-        await waitssh(sshInfo);
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }    
-    console.log(`ssh is ready`);
-    
-    // Run your post-configuration customizations for the Virtual Machine.
-    await postconfiguration();
+        await provider.up(force);
+    } catch (err) {
+        console.log( chalk.red( err.message ) );
+    }
 
-}
-
-async function customize(name)
-{
-    console.log(chalk.keyword('pink')(`Running VM customizations...`));
-}
-
-async function postconfiguration(name)
-{
-    console.log(chalk.keyword('pink')(`Running post-configurations...`));
-     
-    ssh("ls /");
-}
+};
